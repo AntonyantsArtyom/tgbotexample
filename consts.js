5177497111bf0b1c111e4ed56406f7d0779a00c7
AntonyantsArtyom <aa.artem.0303@mail.ru>
2023-08-08 10:47:45 +0300

    port5000

--- consts.js
+++ consts.js
@@ -2,5 +2,5 @@ module.exports = {
    bot_token: "6215621199:AAGD1VWK9kMzkYePnYTprG6Wm6IHfmGfUbM",
    db_url: "mongodb+srv://admin2:admin2@cluster0.dlockzk.mongodb.net/HeroesBurgersCaffee?retryWrites=true&w=majority",
    site: "https://greenfoxbot.ru",
-   port: 80,
+   port: 5000,
 }
