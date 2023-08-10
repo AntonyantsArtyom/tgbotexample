4d2fa0b950cbf31729a30978f66e68c386eda34f
AntonyantsArtyom <aa.artem.0303@mail.ru>
2023-08-10 09:17:45 +0300

    newpass

--- mailer.js
+++ mailer.js
@@ -4,7 +4,7 @@ const transporter = nodemailer.createTransport({
    host: "smtp.mail.ru",
    port: 465,
    secure: true,
-   auth: { user: "greenhungryfox@mail.ru", pass: "mUYHyssGqqbxv2Mm9Rps" },
+   auth: { user: "greenhungryfox@mail.ru", pass: "p6LfwjNvXcHuVFcZ2Rek" },
 })
 
 const mailer = (message) => transporter.sendMail(message)
@@ -12,3 +12,5 @@ const mailer = (message) => transporter.sendMail(message)
 module.exports = mailer
 
 //основной пароль - yellowredbackbackgo123
+
+//mUYHyssGqqbxv2Mm9Rps
